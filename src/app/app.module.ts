1bb78a1e8b27014db7ea57ac1d0c730165d87768
Nervado <evandroabreudeoliveira@gmail.com>
2019-11-22 07:20:34 -0300

    add shopping-cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MenuComponent } from "./restaurant-detail/menu/menu.component";
 import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shopping-cart.component";
 import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.component";
 import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component";
+import { ShoppingCartService } from "./restaurant-detail/shopping-cart/shopping-cart.service";
 
 @NgModule({
   declarations: [
@@ -34,7 +35,7 @@ import { ReviewsComponent } from "./restaurant-detail/reviews/reviews.component"
     ReviewsComponent
   ],
   imports: [BrowserModule, HttpModule, RouterModule.forRoot(ROUTES)],
-  providers: [RestaurantsService],
+  providers: [RestaurantsService, ShoppingCartService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
